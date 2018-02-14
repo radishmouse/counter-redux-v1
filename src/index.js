684cf81cc59e2dfe37426d62a3e7c58c728d55cf
Chris Aquino <chris.aquino@gmail.com>
2018-02-14 14:48:02 -0500

    can add/remove counters

--- src/index.js
+++ src/index.js
@@ -22,21 +22,82 @@ My actions will be:
 { type: 'DECREMENT' }
 */
 
-const actionIncrement = () => {
-  return { type: 'INCREMENT' }
+const actionIncrement = (id) => {
+  return { type: 'INCREMENT', payload: id }
 };
 
-const actionDecrement = () => {
-  return { type: 'DECREMENT' }
+const actionDecrement = (id) => {
+  return { type: 'DECREMENT', payload: id }
+};
+
+const actionAddCounter = () => {
+  return { type: 'ADD_COUNTER' }
+};
+
+const actionRemoveCounter = (whichCounter) => {
+  return { type: 'REMOVE_COUNTER', payload: whichCounter };
 };
 
 const initialState = {
-  count: 0
+  counters: [
+    {
+      count: 0
+    }
+  ]
+};
+
+const counters = (state, action) => {
+  if (state === undefined) {
+    return initialState;
+  }
+
+  switch(action.type) {
+    case 'ADD_COUNTER':
+      console.log('adding counter');
+      return {
+        counters: [
+          ...state.counters,
+          {
+            count: 0
+          }
+        ]
+      }
+    case 'REMOVE_COUNTER':
+      console.log(`removing counter ${action.payload}`);
+      return {
+        counters: state.counters.filter( (c, i) => {
+          return action.payload !== i;
+        })
+      }
+    case 'INCREMENT':
+      console.log(`incrementing counter ${action.payload}`);
+      return {
+        counters: state.counters.map( (c, i) => {
+          if (action.payload === i) {
+            return counter(c, action);
+          }
+          return c;
+        })
+      }
+    case 'DECREMENT':
+      console.log(`decrementing counter ${action.payload}`);
+      return {
+        counters: state.counters.map( (c, i) => {
+          if (action.payload === i) {
+            return counter(c, action);
+          }
+          return c;
+        })
+      }
+    default:
+      return state;
+  }
+
 };
 
 const counter = (state, action) => {
   if (state === undefined) {
-    return initialState;
+    return initialState[0];
   }
 
   switch (action.type) {
@@ -53,30 +114,55 @@ const counter = (state, action) => {
   }
 }
 
-const store = createStore(counter);
+const store = createStore(counters);
 
-store.subscribe( () => {
-  console.log(`The state is now: ${store.getState().count}`);
-  ReactDOM.render(
-    <App
-      count={store.getState().count}
+
+const createCounters = () => {
+  return store.getState().counters.map( (c, i) => {
+    return (<App
+      key={i}
+      count={c.count}
       increment={() => {
-        store.dispatch(actionIncrement())
+        store.dispatch(actionIncrement(i))
       }}
       decrement={ () => {
-        store.dispatch(actionDecrement())
-      } }
-    />, document.getElementById('root'));
+        store.dispatch(actionDecrement(i))
+      }}
+      remove={ () => {
+        store.dispatch(actionRemoveCounter(i))
+      }}
+    />)
+  })
+}
+
+
+
+store.subscribe( () => {
+  console.log(`The state is now: ${store.getState()}`);
+  ReactDOM.render(
+    <div>
+      <button onClick={() => {
+        store.dispatch(actionAddCounter())
+      }}>Add Counter</button>
+      {createCounters()}
+    </div>
+, document.getElementById('root'));
 })
 
 ReactDOM.render(
-  <App
-    count={store.getState().count}
-    increment={() => {
-      store.dispatch(actionIncrement())
-    }}
-    decrement={ () => {
-      store.dispatch(actionDecrement())
-    } }
-  />, document.getElementById('root'));
+  <div>
+      <button onClick={() => {
+        store.dispatch(actionAddCounter())
+      }}>Add Counter</button>
+      {createCounters()}
+  </div>
+, document.getElementById('root'));
 registerServiceWorker();
+
+/*
+      <button onClick={() => {
+        store.dispatch(actionAddCounter())
+      }}>Add Counter</button>
+      {createCounters()}
+
+*/
