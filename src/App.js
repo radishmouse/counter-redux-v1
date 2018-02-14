ed6ae244c9d67b0311f929acd5b8269035c8bf12
Chris Aquino <chris.aquino@gmail.com>
2018-02-14 13:42:40 -0500

    moves redux to index.js, App works off of props

--- src/App.js
+++ src/App.js
@@ -2,73 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { createStore } from 'redux';
-
-/*
-My state should look like this:
-
-{
-  count: 0
-}
-*/
-
-/*
-My actions will be:
-
-{ type: 'INCREMENT' }
-{ type: 'DECREMENT' }
-*/
-
-const actionIncrement = () => {
-  return { type: 'INCREMENT' }
-};
-
-const actionDecrement = () => {
-  return { type: 'DECREMENT' }
-};
-
-const initialState = {
-  count: 0
-};
-
-const counter = (state, action) => {
-  if (state === undefined) {
-    return initialState;
-  }
-
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1
-      }
-    case 'DECREMENT':
-      return {
-        count: state.count - 1
-      }
-    default:
-      return state;
-  }
-}
-
-const store = createStore(counter);
-
-store.subscribe( () => {
-  console.log(`The state is now: ${store.getState().count}`)
-})
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">
-          <button onClick={ () => {
-            store.dispatch(actionDecrement())
-          } }>-</button>
-          0
-          <button onClick={ () => {
-            store.dispatch(actionIncrement())
-          }}>+</button>
+          <button onClick={this.props.decrement}>-</button>
+          {this.props.count}
+          <button onClick={this.props.increment}>+</button>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,79 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+
+/*
+My state should look like this:
+
+{
+  count: 0
+}
+*/
+
+/*
+My actions will be:
+
+{ type: 'INCREMENT' }
+{ type: 'DECREMENT' }
+*/
+
+const actionIncrement = () => {
+  return { type: 'INCREMENT' }
+};
+
+const actionDecrement = () => {
+  return { type: 'DECREMENT' }
+};
+
+const initialState = {
+  count: 0
+};
+
+const counter = (state, action) => {
+  if (state === undefined) {
+    return initialState;
+  }
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+store.subscribe( () => {
+  console.log(`The state is now: ${store.getState().count}`);
+  ReactDOM.render(
+    <App
+      count={store.getState().count}
+      increment={() => {
+        store.dispatch(actionIncrement())
+      }}
+      decrement={ () => {
+        store.dispatch(actionDecrement())
+      } }
+    />, document.getElementById('root'));
+})
+
+ReactDOM.render(
+  <App
+    count={store.getState().count}
+    increment={() => {
+      store.dispatch(actionIncrement())
+    }}
+    decrement={ () => {
+      store.dispatch(actionDecrement())
+    } }
+  />, document.getElementById('root'));
 registerServiceWorker();
